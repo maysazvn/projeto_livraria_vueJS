a4d25c8377336880844e51aaaf47cfda05c3928c
maysazvn <maysabmachado84@gmail.com>
2026-06-12 16:37:39 -0300

    FEAT: função de livros mais vendidos

--- src/components/layout/HomeView.vue
+++ src/components/layout/HomeView.vue
@@ -2,6 +2,12 @@
 // Este arquivo é um componente Vue que representa a página inicial do aplicativo. Ele pode conter uma mensagem de boas-vindas, destaques dos produtos ou qualquer outra informação relevante para os visitantes que acessam o site pela primeira vez. O conteúdo específico da página inicial pode ser personalizado conforme as necessidades do projeto, mas geralmente serve como um ponto de entrada para os usuários explorarem o restante do site. Como sugestão, vocês poderão adicionar uma seção de "Destaques" ou "Novidades" para mostrar os produtos mais recentes ou populares, incentivando os visitantes a navegarem para a página de produtos.
 
 import ProductList from '../products/ProductList.vue';
+import { computed } from 'vue';
+import { produtos } from '@/data/product.js';
+
+const maisVendidos = computed(() => {
+return [...produtos].sort((a, b) => b.vendas - a.vendas).slice(0, 6);
+})
 </script>
 
 <template>
@@ -26,7 +32,7 @@ import ProductList from '../products/ProductList.vue';
    </section>
    <h2>Mais vendidos</h2>
 
-   <ProductList></ProductList>
+   <ProductList :maisvendidos="maisVendidos"></ProductList>
 
   </main>
 
--- src/components/products/ProductList.vue
+++ src/components/products/ProductList.vue
@@ -1,9 +1,15 @@
 <script setup>
 // Este arquivo é um componente Vue que permitirá listar os livros disponíveis para compra. Ele exibirá uma grade de produtos, onde cada produto será representado por um componente ProductCard.vue. O componente ProductList.vue será responsável por buscar os dados dos livros (que estão armazenados em um arquivo JS local - /src/data/products.js). Ele usará um loop para renderizar um ProductCard para cada livro na lista, passando as informações do livro como props para o componente ProductCard. O ProductList.vue é projetado para ser usado na página de listagem de produtos, onde os usuários podem navegar pelos livros disponíveis e clicar em um produto para ver mais detalhes ou adicioná-lo ao carrinho de compras.
 import ProductCard from './ProductCard.vue';
-import { produtos } from '@/data/product.js';
 import { useRoute } from 'vue-router';
+import { produtos } from '@/data/product.js';
 import { computed } from 'vue';
+const props = defineProps(['maisvendidos']);
+
+const listaProdutos = computed(() => {
+return props.maisvendidos ?? produtos;
+}
+)
 
 const route = useRoute()
 
@@ -12,7 +18,7 @@ const produtosFiltrados = computed(() => {
   const min = Number(route.query.precoMin) || 0
   const max = Number(route.query.precoMax) || 9999
   const generoLivro = route.query.genero;
-  return produtos.filter((p) => {
+  return listaProdutos.value.filter((p) => {
     const tituloCerto = p.titulo.toLowerCase().includes(q)
     const precoCerto = p.preco >= min && p.preco <= max
     const generoCerto = !generoLivro || p.genero.toLowerCase() === generoLivro.toLowerCase();
@@ -26,11 +32,11 @@ const produtosFiltrados = computed(() => {
 
 <template>
 <section class="produtos">
-    
+
 
     <ProductCard v-for="livro in produtosFiltrados" :key="livro.id" :id="livro.id" :titulo="livro.titulo" :autor="livro.autor" :resenha="livro.resenha" :preco="livro.preco" :genero="livro.genero" :capa="livro.capa" :subgenero="livro.subgenero"></ProductCard>
-    
-    
+
+
    </section>
 </template>
 
